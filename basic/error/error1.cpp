779fdfcc79c36e1a4001ea3445e9256284fe9d89
jason yien <jason.yien@outlook.com>
2023-12-26 10:22:39 +0800

    error patch

--- basic/error/error1.cpp
+++ basic/error/error1.cpp
@@ -27,15 +27,22 @@ void test(){
    }
 }
 
-//自定义异常 std:exception
+//自定义异常 std::exception
 struct MyException:public exception{
-    //const throw () 不会修改对象的状态且不会抛出任何异常
-    //C++11中引入了noexcept关键字，throw()弃用
-    //const char* what () const throw ()
-    //noexcept 表示函数不会抛出任何异常
-    //noexcept(true) 表示函数不会抛出任何异常
-    //noexcept(false) 表示函数可能会抛出异常
-    //如果函数在noexcept(false)的情况下抛出了异常，则程序将调用std::terminate()，终止程序的执行。
+    /*
+    https://learn.microsoft.com/zh-cn/cpp/cpp/exception-specifications-throw-cpp?view=msvc-170
+    建议只使用noexcept规范
+    c++11，引入noexcept，放到函数()后
+        函数不会抛出任何异常，noexcept(true) 或 noexcept
+        函数可能抛出异常，noexcept(false)
+        函数在noexcept(false)的情况下抛出了异常，则程序将调用std::terminate()，终止程序的执行
+    c++11-，
+        void fun() throw(); //表示fun函数不允许抛出任何异常，即fun函数是异常安全的。
+        void fun() throw(...); //表示fun函数可以抛出任何形式的异常。
+        void fun() throw(exceptionType); //表示fun函数只能抛出exceptionType类型的异常。
+        void fun() throw(int); // 表示只抛出int类型异常
+        void fun() throw(int，char); // 表示抛出in，char类型异常
+    */
     const char* what () const noexcept
     {
         return "C++ My exception";
@@ -57,13 +64,13 @@ void test1(){
 }
 
 class MyException2 : public exception {
+private:
+    string message_;
 public:
     MyException2(const string& message) : message_(message) {}
     const char* what() const throw() {
         return message_.c_str();
     }
-private:
-    string message_;
 };
 
 void test2() {
